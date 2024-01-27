b41e9bb653ba15782d81dc3dda83177e728ffbb6
Patrick Munuku <wafulapatrick6@gmail.com>
2024-01-27 19:30:39 +0300

    Complete Order Summary component

--- src/components/checkoutCard/CheckoutCard.jsx
+++ src/components/checkoutCard/CheckoutCard.jsx
@@ -1,13 +1,13 @@
-// import BillingInformation from "../billingInformation/BillingInformation";
+import BillingInformation from "../billingInformation/BillingInformation";
 // import CardInformation from "../cardInformation/CardInformation";
 // import OrderSummary from "../orderSummary/OrderSummary";
-import CompletedPayment from "../completedPayment/CompletedPayment";
+// import CompletedPayment from "../completedPayment/CompletedPayment";
 import "./checkoutCard.scss";
 
 const CheckoutCard = () => {
   return (
     <div>
-      <CompletedPayment />
+      <BillingInformation />
     </div>
   );
 };
--- src/components/orderSummary/OrderSummary.jsx
+++ src/components/orderSummary/OrderSummary.jsx
@@ -3,13 +3,44 @@ import "./orderSummary.scss";
 import { ShopContext } from "../../context/ShopContext";
 
 const OrderSummary = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { items, cartItems } = useContext(ShopContext);
 
   return (
     <div className="order-summary">
-      {Array(cartItems).map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      <h1>ORDER SUMMARY</h1>
+      <div className="underline"></div>
+      <div className="order-summary-container">
+        {items.slice(0, 4).map((item) => {
+          if (cartItems[item.id] !== 0) {
+            return (
+              <div key={item.id} className="cart-item-container">
+                <img src={item.image} alt={`cart item ${item.id}`} />
+                <div className="cart-item-details">
+                  <h4>{item.title}</h4>
+                  <p>Quantity: 2</p>
+                  <h6>{item.price}</h6>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="lower-section">
+        <hr />
+        <div className="sub-total">
+          <p>Sub Total:</p>
+          <span>00</span>
+        </div>
+        <div className="delivery-charge">
+          <p>Delivery Charge:</p>
+          <span>00</span>
+        </div>
+        <hr />
+        <div className="total-amount">
+          <p>Total Amount:</p>
+          <span>00</span>
+        </div>
+      </div>
     </div>
   );
 };
