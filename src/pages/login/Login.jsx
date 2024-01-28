bd5a0ae306aca16243a3846a51c7737dd74d1c7d
Patrick Munuku <wafulapatrick6@gmail.com>
2024-01-28 18:56:12 +0300

    complete login and signup pages layout

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,46 @@
+import { LuUser2 } from "react-icons/lu";
+import { LiaUnlockAltSolid } from "react-icons/lia";
 import "./login.scss";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="login-container">
+        <h1 className="login-head">SIGN IN</h1>
+        <p className="login-descrip">To manage your order please login first</p>
+        <form action="" className="login-form">
+          <fieldset>
+            <legend>User Name or Email</legend>
+            <input type="text" />
+            <LuUser2 className="user-icon" />
+          </fieldset>
+          <fieldset>
+            <legend>Password</legend>
+            <input type="text" />
+            <LiaUnlockAltSolid className="lock-icon" />
+          </fieldset>
+          <div className="redirection-links">
+            <p>
+              Forgot Password?{" "}
+              <Link to="#" className="redirection-link">
+                Recover
+              </Link>{" "}
+            </p>
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="redirection-link">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,7 +1,65 @@
+import { Link } from "react-router-dom";
+import { LuUser2 } from "react-icons/lu";
+import { LiaUnlockAltSolid } from "react-icons/lia";
+import { MdEmail } from "react-icons/md";
+import { LuAsterisk } from "react-icons/lu";
 import "./signup.scss";
 
 const SignUp = () => {
-  return <div className="signup">SignUp</div>;
+  return (
+    <div className="signup">
+      <div className="signin-container">
+        <h1>SIGN UP</h1>
+        <p>It&apos;s easy to create account</p>
+        <form action="">
+          <fieldset>
+            <legend>
+              Full Name <LuAsterisk className="asterisk" />
+            </legend>
+            <input type="text" />
+            <LuUser2 className="user-icon" />
+          </fieldset>
+          <fieldset>
+            <legend>
+              User Name <LuAsterisk className="asterisk" />
+            </legend>
+            <input type="text" />
+            <LuUser2 className="user-icon" />
+          </fieldset>
+          <fieldset>
+            <legend>
+              Email <LuAsterisk className="asterisk" />
+            </legend>
+            <input type="email" />
+            <MdEmail className="lock-icon" />
+          </fieldset>
+          <fieldset>
+            <legend>
+              Password <LuAsterisk className="asterisk" />
+            </legend>
+            <input type="password" />
+            <LiaUnlockAltSolid className="lock-icon" />
+          </fieldset>
+          <fieldset>
+            <legend>
+              Confirm Password <LuAsterisk className="asterisk" />
+            </legend>
+            <input type="text" />
+            <LiaUnlockAltSolid className="lock-icon" />
+          </fieldset>
+          <p>
+            Have an account?{" "}
+            <Link to="/login" className="redirection-link">
+              Sign In
+            </Link>
+          </p>
+          <button type="submit" className="signin-btn">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
